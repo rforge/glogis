bc5927c914ec8a0eba8b6140bdec6be355500875
azeileis <azeileis@b679b2f7-6539-45d0-a605-5ddcb976cde9>
2011-06-12 19:41:46 +0000

    updated WWW information for upcoming release of working paper
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/glogis@119 b679b2f7-6539-45d0-a605-5ddcb976cde9

--- www/index.php
+++ www/index.php
@@ -22,7 +22,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+<a href="http://R-Forge.R-project.org/"><img src="<?php echo $themeroot; ?>/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
 
 
@@ -32,23 +32,28 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <p>R package for fitting and testing generalized logistic distributions (type I).</p>
 
 <ul>
-  <li><a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name;?>/">R-Forge summary page</a></li>
-  <li>R package: [<a href="https://R-Forge.R-project.org/src/contrib/glogis_0.0-4.tar.gz">source package</a>]
-      [<a href="https://R-Forge.R-project.org/bin/windows/contrib/latest/glogis_0.0-4.zip">Windows binary</a>]
-      [<a href="https://R-Forge.R-project.org/bin/macosx/leopard/contrib/latest/glogis_0.0-4.tgz">MacOS X binary</a>]
+  <li>Stable release version: <a href="http://CRAN.R-project.org/package=glogis">[CRAN]</a></li>
+  <li>Development version: <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name;?>/">[R-Forge]</a></li>
 </ul>
 
 
 <h2>Structural Breaks in Inflation Dynamics within the European Monetary Union</h2>
 <h3>Thomas Windberger, Achim Zeileis</h3>
 
-<p>Investigation of European inflation rates based on Harmonized Index of Consumer Prices (HICP).
-Inflation series are estimated by generalized logistic distribution model, tested for structural breaks, and (if any) breakpoints
-are determined.</p>
+<p>To assess the effects of the EMU on inflation rate dynamics of
+  its member states, the inflation rate series for 21~European countries
+  are investigated for structural changes. To capture changes in mean,
+  variance, and skewness of inflation rates, a generalized logistic
+  model is adopted and complemented with structural break tests and
+  breakpoint estimation techniques. These reveal considerable differences
+  in the patterns of inflation dynamics and the structural changes
+  therein. Overall, there is a convergence towards a lower mean inflation rate
+  with reduced skewness, but it is accompanied by an increase in variance.</p>
 
 <ul>
-  <li><a href="https://R-Forge.R-project.org/scm/viewvc.php/*checkout*/pkg/paper/Slides/slides.pdf?root=glogis">Slides</a></li>
-  <li><a href="https://R-Forge.R-project.org/scm/viewvc.php/*checkout*/pkg/paper/Paper/draft.pdf?root=glogis">Manuscript draft</a></li>
+  <li>Working paper: <a href="http://EconPapers.RePEc.org/RePEc:inn:wpaper:2011-12">[pdf]</a></li>
+  <li>Replication files: See <tt>demo(package = "glogis")</tt>.</li>
+<!--  <li><a href="https://R-Forge.R-project.org/scm/viewvc.php/*checkout*/pkg/paper/Slides/slides.pdf?root=glogis">Slides</a></li> -->
 </ul>
 
 </body>
